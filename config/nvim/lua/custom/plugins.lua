6761d4c4f1841268cd73efc42e73fae07df054d0
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2023-07-12 14:07:14 -0600

    add nvchad fixed

---
+++ config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,94 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "pyright",
+        "mypy",
+        "ruff",
+        "debugpy",
+        "gopls",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function ()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function (_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function (_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end
+  },
+  {
+    "vim-crystal/vim-crystal",
+    ft = "crystal",
+    config = function (_)
+      vim.g.crystal_auto_format = 1
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function (_)
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"go", "python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function (_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build= function ()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+}
+return plugins

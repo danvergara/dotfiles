d76903434362ab5025f72f809dda67d9bf4dfa6e
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2025-05-07 18:37:51 -0600

    show diagnostics

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -85,3 +85,22 @@ lspconfig.helm_ls.setup {
     },
   },
 }
+
+vim.diagnostic.config {
+  underline = false,
+  virtual_text = {
+    spacing = 2,
+    prefix = "●",
+  },
+  update_in_insert = false,
+  severity_sort = true,
+  signs = {
+    text = {
+      -- Alas nerdfont icons don't render properly on Medium!
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+}
